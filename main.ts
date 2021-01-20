aab076ccb79809ce232b2e41cfd6d5bf68e3a558
Alexandre Dumont <adumont@users.noreply.github.com>
2021-01-20 01:40:06 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -50,6 +50,32 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Monkey, function (sprite, otherS
     }
     otherSprite.destroy(effects.disintegrate, 200)
 })
+function newEgg () {
+    if (sprites.allOfKind(SpriteKind.Monkey).length < maxMonkeys) {
+        anEgg = sprites.create(img`
+            . . . . . . b d d 1 . . . . . . 
+            . . . . . b d d d d 1 . . . . . 
+            . . . . b d d d d d d 1 . . . . 
+            . . . b d d d d d d d d 1 . . . 
+            . . . b d d d d d d d d 1 . . . 
+            . . b d d d d d d d d d d 1 . . 
+            . . b d d d d d d d d d d 1 . . 
+            . . b d d d d d d d d d d 1 . . 
+            . . b d d d d d d d d d d 1 . . 
+            . . b d d d d d d d d d d 1 . . 
+            . . b d d d d d d d d d d 1 . . 
+            . . b d d d d d d d d d d 1 . . 
+            . . b d d d d d d d d d d 1 . . 
+            . . b b d d d d d d d d d 1 . . 
+            . . . b b d d d d d d 1 1 . . . 
+            . . . . b b d d d d 1 1 . . . . 
+            `, SpriteKind.Egg)
+        tiles.placeOnRandomTile(anEgg, myTiles.tile3)
+        anEgg.setFlag(SpriteFlag.BounceOnWall, true)
+        anEgg.vy = -150
+        anEgg.ay = 150
+    }
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.vy == 0 && !(mySprite.tileKindAt(TileDirection.Center, myTiles.tile5))) {
         mySprite.vy = -150
@@ -83,11 +109,61 @@ function createPlayer () {
     mySprite.ay = 300
 }
 let mySprite: Sprite = null
+let anEgg: Sprite = null
 let aMonkey: Sprite = null
 let maxMonkeys = 0
 let Level = 0
-let anEgg = null
 Level = 0
 info.setScore(0)
 info.setLife(4)
 createLevel()
+// Monkey logic
+game.onUpdate(function () {
+    for (let value3 of sprites.allOfKind(SpriteKind.Monkey)) {
+        if (Math.round(value3.x % 2) == 1) {
+            value3.setImage(img`
+                . . . . f f f f f . . . . . . . 
+                . . . f e e e e e f . . . . . . 
+                . . f d d d d e e e f . . . . . 
+                . c d f d d f d e e f f . . . . 
+                . c d f d d f d e e d d f . . . 
+                c d e e d d d d e e b d c . . . 
+                c d d d d c d d e e b d c . . . 
+                c c c c c d d e e e f c . . . . 
+                . f d d d d e e e f f . . . . . 
+                . . f f f f f e e e e f . . . . 
+                . . . . f f e e e e e e f . f f 
+                . . . f e e f e e f e e f . e f 
+                . . f e e f e e f e e e f . e f 
+                . f b d f d b f b b f e f f e f 
+                . f d d f d d f d d b e f f f f 
+                . . f f f f f f f f f f f f f . 
+                `)
+        } else {
+            value3.setImage(img`
+                . . . . f f f f f . . . . . . . 
+                . . . f e e e e e f . . . . . . 
+                . . f d d d d e e e f f . . . . 
+                . c d d d d d d e e d d f . . . 
+                . c d f d d f d e e b d c . . . 
+                c d d f d d f d e e b d c . f f 
+                c d e e d d d d e e f c . f e f 
+                c d d d d c d e e e f . . f e f 
+                . f c c c d e e e f f . . f e f 
+                . . f f f f f e e e e f . f e f 
+                . . . . f e e e e e e e f f f . 
+                . . f f e f e e f e e e e f . . 
+                . f e f f e e f f f e e e f . . 
+                f d d b d d c f f f f f f b f . 
+                f d d c d d d f . . f c d d f . 
+                . f f f f f f f . . . f f f . . 
+                `)
+        }
+        if (value3.vx > 0) {
+            value3.image.flipX()
+        }
+        if (value3.tileKindAt(TileDirection.Bottom, myTiles.transparency16) || value3.tileKindAt(TileDirection.Bottom, myTiles.tile8)) {
+            value3.vx = 0 - value3.vx
+        }
+    }
+})
